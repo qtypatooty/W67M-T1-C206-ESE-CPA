3e040a1c8f3fd1e12f9046809e79a692e392ca5f
huihu <20032316@myrp.edu.sg>
2021-08-04 22:17:59 +0800

    edit career class

--- C206_CaseStudy/src/career.java
+++ C206_CaseStudy/src/career.java
@@ -18,32 +18,28 @@ public class career {
 		return career_id;
 	}
 
-//	public void setCareer_id(int career_id) {
-//		this.career_id = career_id;
-//	}
-
 	public String getCareer_name() {
 		return career_name;
 	}
 
-//	public void setCareer_name(String career_name) {
-//		this.career_name = career_name;
-//	}
+	public void setCareer_name(String career_name) {
+		this.career_name = career_name;
+	}
 
 	public String getCareer_info() {
 		return career_info;
 	}
 
-//	public void setCareer_info(String career_info) {
-//		this.career_info = career_info;
-//	}
+	public void setCareer_info(String career_info) {
+		this.career_info = career_info;
+	}
 
 	public String getCareer_jobs() {
 		return career_jobs;
 	}
 
-//	public void setCareer_jobs(String career_jobs) {
-//		this.career_jobs = career_jobs;
-//	}
+	public void setCareer_jobs(String career_jobs) {
+		this.career_jobs = career_jobs;
+	}
 
 }
